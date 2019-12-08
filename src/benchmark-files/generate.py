f2cefc1665985a225fa782ba99a616b620fffb2b
Sailun Xu <sailunx@andrew.cmu.edu>
2019-12-08 14:06:36 -0500

    add ring test case

--- src/benchmark-files/generate.py
+++ src/benchmark-files/generate.py
@@ -7,18 +7,25 @@ def random(size):
         ys = np.random.uniform(-10,10,size)
         for x,y in zip(xs,ys):
             f.write("{} {}\n".format(x,y))
-        points = np.array([xs,ys]).T
-        # clustering
 
 def mixture(size):
     with open("mixture-{}.in".format(size), 'w') as f:
         pass
 
 def rings(size):
-    with open("rings-{}.in".format(size), 'w') as f:
-        pass
+    radius = 8
+    noise_num = int(size * 0.1)
+    ring_num = size - noise_num
+    ns = np.random.normal(0, 1, (ring_num, 2))
+    ns = ns/np.linalg.norm(ns, axis=1, keepdims=True)
+    rs = radius + np.random.normal(0, 0.5, (ring_num, 1))
+    noises = np.random.uniform(-10,10,(noise_num, 2))
+    points = np.concatenate([ns * rs, noises], axis=0)
+    with open("rings-{}.in".format(size), 'w') as f: 
+        for x,y in points:
+            f.write("{} {}\n".format(x,y))
 
-all_cases = [random, mixture, rings]
+all_cases = [mixture, rings]
 
 def main():
     for case in all_cases:
