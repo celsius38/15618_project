36c9733a745b847923c320c3448b0df4a61013a4
Tammy <liuyueni07@gmail.com>
2019-12-09 14:45:31 -0500

    Label Non-core Cells

--- src/dbscan-mpi.cpp
+++ src/dbscan-mpi.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <vector>
 #include <queue>
+#include <unordered_map>
 
 #define MASTER 0
 
@@ -49,21 +50,31 @@ public:
         vector<size_t> local_adj_list;
         size_t local_cell_count;
         vector<Cell> local_partition;
-        ///////////////////////////////////////////////////
-        // TODO: insert manual testing
-        ///////////////////////////////////////////////////
+
+        size_t local_point_count;
+        vector<size_t> local_point_id;
+        vector<short> local_point_is_core;
 
         if(taskid != MASTER) {
+            // send cell graph
             MPI_Send(&local_adj_list_len, 1, MPI_UNSIGNED_LONG, MASTER, 1, MPI_COMM_WORLD);
             MPI_Send(&local_adj_list[0], local_adj_list_len, MPI_UNSIGNED_LONG, MASTER, 2, MPI_COMM_WORLD);
             MPI_Send(&local_cell_count, 1, MPI_UNSIGNED_LONG, MASTER, 3, MPI_COMM_WORLD);
             MPI_Send(&local_partition[0], local_cell_count, MPI_Cell, MASTER, 4, MPI_COMM_WORLD);
+
+            // send point_is_core
+            MPI_Send(&local_point_count, 1, MPI_UNSIGNED_LONG, MASTER, 5, MPI_COMM_WORLD);
+            MPI_Send(&local_point_id[0], local_point_count, MPI_UNSIGNED_LONG, MASTER, 6, MPI_COMM_WORLD);
+            MPI_Send(&local_point_is_core[0], local_point_count, MPI_SHORT, MASTER, 7, MPI_COMM_WORLD);
         }
 
         if(taskid == MASTER) {
+            // key is point id, value is is_core
+            unordered_map<size_t, short> point_is_core_map; 
+            addIntoMap(point_is_core_map, local_point_id, local_point_is_core);
             // TODO: tree like merge
             for(int i = 1; i < numtasks; i++) {
-                // TODO: message passing from a task
+                // receive a cell graph
                 size_t other_adj_list_len;
                 vector<size_t> other_adj_list;
                 size_t other_cell_count;
@@ -85,6 +96,20 @@ public:
                     merged_partition, merged_adj_list);
                 local_partition = merged_partition;
                 local_adj_list = merged_adj_list;
+
+                // receive point_is_core
+                size_t other_point_count;
+                vector<size_t> other_point_id;
+                vector<short> other_point_is_core;
+
+                MPI_Recv(&other_point_count, 1, MPI_UNSIGNED_LONG, status.MPI_SOURCE, 5, MPI_COMM_WORLD, &status);
+                other_point_id.resize(other_point_count);
+                other_point_is_core.resize(other_point_count);
+                MPI_Recv(&other_point_id[0], other_point_count, MPI_UNSIGNED_LONG, status.MPI_SOURCE, 6, MPI_COMM_WORLD, &status);
+                MPI_Recv(&other_point_is_core[0], other_point_count, MPI_SHORT, status.MPI_SOURCE, 7, MPI_COMM_WORLD, &status);
+
+                // TODO add into point_is_core_map
+                addIntoMap(point_is_core_map, other_point_id, other_point_is_core);
             }
             // local_partition and local_adj_list for master now is the global graph
             vector<int> cell_cluster_id(local_partition.size(), 0);
@@ -92,23 +117,24 @@ public:
             labelPointsInCoreCells(cell_cluster_id, labels);
             // TODO label points in non core cells
             // TODO combine and get global point_is_core
-            /*
-            vector<int> point_is_core;
-            labelPointsInNonCoreCells(cell_cluster_id, point_is_core, labels);
-            */
+            labelPointsInNonCoreCells(cell_cluster_id, point_is_core_map, labels);
         }
 
         MPI_Type_free(&MPI_Cell);
         MPI_Finalize();
-        // TODO return cluster_count
-        return 0;
+        return cluster_count;
     }
 
 private:
-    size_t* point_index; // len = number of points
-    size_t* bin_index; // len = number of points
-    size_t* bin_start_index; // len = number of bins
-    size_t* bin_end_index; // len = number of bins
+    // TODO: real
+    size_t point_index[10] = {1,8,5,7,2,3,4,9,0,6}; // len = number of points
+    size_t bin_index[10] = {0,0,1,1,2,2,3,3,4}; // len = number of points
+    size_t bin_start_index[5] = {0,2,4,6,8}; // len = number of bins
+    size_t bin_end_index[5] = {2,4,6,8,10}; // len = number of bins
+    int col_bins;
+    int row_bins;
+    vector<Vec2> points;
+    float eps;
 
     void createMPICell() {
         MPI_Datatype oldtypes[2] = {MPI_SHORT, MPI_UNSIGNED_LONG};
@@ -127,12 +153,18 @@ private:
         MPI_Type_commit(&MPI_Cell);
     }
 
+    void addIntoMap(unordered_map<size_t, short> point_is_core_map, vector<size_t> point_id, vector<short> point_is_core) {
+        for(int i = 0; i < point_id.size(); i++) {
+            point_is_core_map[point_id[i]] = point_is_core[i];
+        }
+    }
 
     // add a cell into the merged graph
     void addCell(Cell cell, vector<Cell>& all_cells, vector<size_t>& adj_list_sub, vector<size_t>& adj_list) {
         Cell new_cell;
         new_cell.id = cell.id;
         new_cell.degree = cell.degree;
+        new_cell.is_core = cell.is_core;
         if(all_cells.size() == 0) {
             new_cell.start_index = 0;
         } else {
@@ -229,7 +261,7 @@ private:
     }
 
     void labelPointsInNonCoreCells(vector<int>& cell_cluster_id, 
-                                    vector<int>& point_is_core,
+                                    unordered_map<size_t, short> point_is_core_map,
                                     vector<int>& labels) {
         for(int cell_id = 0; cell_id < cell_cluster_id.size(); cell_id++) {
             if(cell_cluster_id[cell_id] > 0) {
@@ -238,11 +270,11 @@ private:
                     size_t point_id = point_index[i];
                     // label one point
                     // TODO
-                    vector<size_t> neighbours = findNeighbour(point_id);
+                    vector<size_t> neighbours = findNeighbours(point_id, cell_id);
                     for(size_t neighbour_index = 0; neighbour_index < neighbours.size(); neighbour_index++) {
                         size_t neighbour_id = neighbours[neighbour_index];
                         // neighbour is core
-                        if(point_is_core[neighbour_id] == 1) {
+                        if(point_is_core_map[neighbour_id] == 1) {
                             labels[point_id] = labels[neighbour_id];
                             break;
                         }
@@ -251,9 +283,29 @@ private:
             }
         }
     }
+    
+    vector<size_t> findNeighbours(size_t point_id, int cell_id) {
+        vector<size_t> neighbours;
+        int cell_row_id = cell_id/col_bins;
+        int cell_col_id = cell_id%col_bins;
+        for(int row_diff = -2; row_diff < 3; ++row_diff) {
+            for(int col_diff = -2; col_diff < 3; ++col_diff) {
+                addOneCellNeighbours(neighbours, point_id, cell_row_id+row_diff, cell_col_id+col_diff);
+            }
+        }
+    }
 
-    // TODO
-    vector<size_t> findNeighbour(size_t point_id) {
+    void addOneCellNeighbours(vector<size_t> neighbours, size_t point_id, int cell_row_id, int cell_col_id) {
+        if(cell_row_id < 0 || cell_row_id > row_bins || cell_col_id < 0 || cell_col_id > col_bins) {
+            return;
+        }
+        int cell_id = cell_row_id * col_bins + cell_col_id;
+        for(size_t i = bin_start_index[cell_id]; i < bin_end_index[cell_id]; i++) {
+            size_t other_point_id = point_index[i];
+            if((points[other_point_id]-points[point_id]).length() <= eps) {
+                neighbours.push_back(other_point_id);
+            }
+        }
     }
 };
 
